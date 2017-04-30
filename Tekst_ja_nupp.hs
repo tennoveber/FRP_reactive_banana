7326065247d2fe9ef1b4d6b0341e7bcf39349ca5
tennoveber <eestiruulibhehe@gmail.com>
2017-04-30 11:01:16 +0300

    Add files via upload

---
+++ Tekst_ja_nupp.hs
@@ -0,0 +1,32 @@
+
+{-# LANGUAGE ScopedTypeVariables #-}
+import Graphics.UI.WX hiding (Event)
+import Reactive.Banana
+import Reactive.Banana.WX
+
+gui :: IO ()
+gui = do
+    f         <- frame [text := "Tekst ja nupp"]
+    input     <- entry f []
+    output    <- staticText f []
+    but       <- button f [text := "Vajuta mind"]
+    
+    set f [layout := margin 10 $ row 10 [widget input
+        , widget but, minsize (sz 70 20) $ widget output]]
+
+    let networkDescription :: MomentIO ()
+        networkDescription = do
+
+            tekst <- behaviorText input ""
+            but2 <- event0 but command
+
+            (kaitumine :: Behavior String) 
+                <- stepper "" (tekst <@ but2)
+
+            sink output [text :== kaitumine]   
+
+    network <- compile networkDescription    
+    actuate network
+
+main :: IO ()
+main = start gui
